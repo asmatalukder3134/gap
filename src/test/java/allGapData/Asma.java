999d1dd4bede579a62a5ca5a02326e286187ff32
asmatalukder3134 <asma.talukder@yahoo.com>
2023-11-13 11:51:11 -0600

    all gap

--- src/test/java/allGapData/Asma.java
+++ src/test/java/allGapData/Asma.java
@@ -2,10 +2,16 @@ package allGapData;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-public class Asma extends Homes{
+public class Asma extends Homes {
 	void vv() {
 		int xx;
 	}
@@ -21,7 +27,7 @@ public class Asma extends Homes{
 //       Asma aa= (Asma)hm;
 //       aa.vv();
 //    }
-		//public static void main(String[] args) {
+	// public static void main(String[] args) {
 //			int dayOfWeek = 2  ;
 //	    	String day;
 //	     	switch (dayOfWeek) {
@@ -39,30 +45,52 @@ public class Asma extends Homes{
 //	    	}
 //	     	System.out.println(day);
 //			
-			
+
 	public static void main(String[] args) {
-		Integer []a= {2,4,3,5,7,6,9,8,77,99,55};
-		List la=Arrays.asList(a);
-		//la.remove(1);
-		System.out.println(la);
-		
-		List li=new ArrayList();
-		li.add(10); 
+		Integer[] a = { 2, 4, 3, 5, 7, 6, 9, 8, 77, 99, 55 };
+		List la = Arrays.asList(a);
+		// la.addAll(a);
+		// la.remove(1);
+		// System.out.println(la);
+
+		List li = new ArrayList();
+		li.add(10);
 		li.add(20);
-		li.add(la);
-		li.remove(0);
-		
- 
+//		li.add("asma");
+//		li.add("lima");
+//		li.add("lima");
+//		li.add("ema");
+		li.add(20);
+		li.add(30);
+		Collections.sort(li);
+		// System.out.println("index three"+li.get(3));
+		for (Object ob : li) {
+			// System.out.println("ob"+ob);
+		}
 
-	    	//System.out.println(li);
-	    
-	    
-		//Collections.sort(li);
-		//Collections.reverse(li);
-		
-	
-		
-    			
-	}}
+		// Collections.sort(li);
+		// Collections.reverse(li);
 
-
+		Map map = new HashMap();
+		map.put(1, "cake");
+		map.put(2, "fake");
+		map.put(3, "lake");
+		map.put(4, "cake");
+		map.put(5, "cake");
+		Set set = map.keySet();
+		Collection c = map.values();
+		for (Object obb : c) {
+			System.out.println("obb:" + obb);
+		}
+		Iterator it = set.iterator();
+		while (it.hasNext()) {
+			// System.out.println(it.next());
+		}
+List l=new ArrayList();
+//l.add(5, "break");
+l.addAll(c);
+for(Object o:l) {
+System.out.println("oo:"+o);
+}
+	}
+}
--- src/test/java/allGapData/Foo.java
+++ src/test/java/allGapData/Foo.java
@@ -2,6 +2,7 @@ package allGapData;
 
 public interface Foo {
 int x=10;
+
 public class Test { 
     public static void main(String[] args) { 
         //Foo.x = 20; %%%%%%%%%%%interface variable final thake, can't change 
